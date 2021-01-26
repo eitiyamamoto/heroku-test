463e740842036055d22f02546a1436f80b620250
eitiyamamoto <eitiyamamoto@gmail.com>
2021-01-26 14:45:28 -0300

    Initial Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name herokuAppApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the herokuAppApp
+ */
+angular.module('herokuAppApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
